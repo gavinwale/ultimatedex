4e9e11cec7c3d32d32ad9e7d545dce79698efd3f
Gavin <wale.gavin@gmail.com>
2023-03-13 23:35:30 -0600

    get out of here

--- interface-main/src/utils/urlChecks.test.ts
+++
@@ -1,6 +0,0 @@
-import { hasURL } from './urlChecks'
-
-test('hasURL', () => {
-  expect(hasURL('this is my personal website: https://www.example.com')).toBe(true)
-  expect(hasURL('#corngang')).toBe(false)
-})
